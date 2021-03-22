3d47b8aa823d6628b9759c2c7defa634428e37ca
waterLover <lijiandong121@163.com>
2021-03-22 12:23:19 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+
+import Phaser from 'phaser';
+class MyGame extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('');
+}
+
+    preload ()
+    {   
+        this.load.image('logo', require('./assets/logo.png'));
+    }
+      
+    create ()
+    {
+        const logo = this.add.image(400, 150, 'logo');
+      
+        this.tweens.add({
+            targets: logo,
+            y: 450,
+            duration: 2000,
+            ease: "Power2",
+            yoyo: true,
+            loop: -1
+        });
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: MyGame
+};
+
+const game = new Phaser.Game(config);
